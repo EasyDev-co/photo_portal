26fece2479e09f8ed084902c4e2f2d96e7ed36f3
Elena Vagner <develop.vagner@gmail.com>
2024-06-06 12:57:53 +0700

    PP-3: Реализована админ панель

--- backend/apps/kindergarten/admin.py
+++ backend/apps/kindergarten/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.kindergarten.models.region import Region
+from apps.kindergarten.models.kindergarten import Kindergarten
+from apps.kindergarten.models.photo_price import PhotoPrice, PhotoType
+
+
+@admin.register(Kindergarten)
+class KindergartenAdmin(admin.ModelAdmin):
+    list_display = (
+        'region',
+        'name',
+        'code',
+        'qr_code',
+        'has_photobook'
+    )
+    list_filter = ('region',)
+    search_fields = ('name', 'code')
+    readonly_fields = ('qr_code',)
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ransom_amount')
+
+
+@admin.register(PhotoPrice)
+class PhotoPriceAdmin(admin.ModelAdmin):
+    list_display = ('price', 'photo_type', 'region')
