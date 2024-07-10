cc71bd90e220ef92c6270e846db480bc04617402
loach99 <constantinent@mail.ru>
2024-07-10 19:24:58 +0300

    PP-43: внес мелкие фиксы

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -3,46 +3,47 @@ import { setCookie } from "../utils/setCookie";
 
 const authSlice = createSlice({
     name: 'user',
-    initialState:{
+    initialState: {
         email: null,
         access: true,
         refresh: null,
-        blur:false,
+        blur: false,
         code: null,
-        photos:[],
-        photosLine:[]
+        photos: [],
+        photosLine: []
     },
-    reducers:{
-        setUser:(state,action)=>{
-            setCookie('refresh',action.payload.refresh);
+    reducers: {
+        setUser: (state, action) => {
+            setCookie('refresh', action.payload.refresh);
             state.access = action.payload.access;
-            state.refresh= action.payload.refresh;
+            state.refresh = action.payload.refresh;
         },
-        removeUser:(state) =>{
+        removeUser: (state) => {
             state.email = null;
             state.access = null;
         },
-        addBlur:(state, action)=>{
+        addBlur: (state, action) => {
             state.blur = action.payload;
         },
-        setEmail(state, action){
+        setEmail(state, action) {
             state.email = action.payload;
         },
-        setCode(state,action){
+        setCode(state, action) {
             state.code = action.payload.code;
         },
-        setAccessToken(state,action){
+        setAccessToken(state, action) {
             state.access = action.payload;
         },
-        addPhotos(state,action){
+        addPhotos(state, action) {
             state.photos.push(action.payload);
         },
-        addPhotoLine(state,action){
+        addPhotoLine(state, action) {
             state.photosLine = action.payload;
         }
+
     }
 });
 
-export const { setUser, removeUser,addBlur,setEmail, setCode, setAccessToken,addPhotos, addPhotoLine} = authSlice.actions;
+export const { setUser, removeUser, addBlur, setEmail, setCode, setAccessToken, addPhotos, addPhotoLine } = authSlice.actions;
 
 export default authSlice.reducer;
