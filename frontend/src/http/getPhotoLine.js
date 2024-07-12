95ff89c73326b227f3f5c4a4198e5bb0389aacd2
AlexanderPAI <lawlikeart@yandex.ru>
2024-07-12 16:30:43 +0300

    PP-47: в http в URLах удалены порты

--- frontend/src/http/getPhotoLine.js
+++ frontend/src/http/getPhotoLine.js
@@ -2,7 +2,7 @@ import { setCookie } from "../utils/setCookie";
 import { tokenRefreshCreate } from "./tokenRefreshCreate";
 
 export const getPhotoLine = async (id, access) => {
-    const url = `https://photodetstvo.easydev-program.com:8080/api/v1/photo/photo_line/${id}/`;
+    const url = `https://photodetstvo.easydev-program.com/api/v1/photo/photo_line/${id}/`;
 
     const response = await fetch(url, {
         headers: {
