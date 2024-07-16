5ad287034128c89af8835f14adea1f4f57ea79df
loach99 <constantinent@mail.ru>
2024-07-16 21:10:10 +0300

    PP-76: внес фиксы

--- frontend/src/http/parentChangePass.js
+++ frontend/src/http/parentChangePass.js
@@ -1,5 +1,5 @@
 export const parentChangePass = async (code,email,newPass) => {
-    const url = 'https://photodetstvo.easydev-program.com/://127.0.0.1:8080/api/v1/parent/change_password/';
+    const url = 'https://photodetstvo.easydev-program.com/api/v1/parent/change_password/';
 
     const sendData = {
         email: email,
