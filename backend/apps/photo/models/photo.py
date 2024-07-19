f42e03d2f56c28a9bec5739d5fdc20b4e1cabdbc
Elena Vagner <develop.vagner@gmail.com>
2024-07-19 17:27:33 +0700

    PP-18: Добавлено поле serial_number у Photo; добавлена валидация уникальности номеров serial_number в одной фотолинии  для Photo

--- backend/apps/photo/models/photo.py
+++ backend/apps/photo/models/photo.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from .photo_line import PhotoLine
 from apps.utils.models_mixins.models_mixins import UUIDMixin
+
+
+class SerialPhotoNumber(models.IntegerChoices):
+    """
+    Порядковый номер фотографии в фотолинии.
+    """
+    photo_1 = 1, 'Фото 1'
+    photo_2 = 2, 'Фото 2'
+    photo_3 = 3, 'Фото 3'
+    photo_4 = 4, 'Фото 4'
+    photo_5 = 5, 'Фото 5'
+    photo_6 = 6, 'Фото 6'
 
 
 class Photo(UUIDMixin):
@@ -20,9 +33,27 @@ class Photo(UUIDMixin):
         verbose_name='Фотография'
     )
 
+    serial_number = models.PositiveSmallIntegerField(
+        choices=SerialPhotoNumber.choices,
+        verbose_name='Порядковый номер'
+    )
+
     def __str__(self):
         return f'Фото №{self.number}'
 
     class Meta:
         verbose_name = 'Фотография'
         verbose_name_plural = 'Фотографии'
+
+    def clean(self):
+        super().clean()
+
+        photos_from_photo_line = Photo.objects.filter(photo_line=self.photo_line.id)
+
+        if self.pk:
+            photos_from_photo_line = photos_from_photo_line.exclude(pk=self.pk)
+
+        existing_serial_numbers = photos_from_photo_line.values_list('serial_number', flat=True)
+
+        if self.serial_number in existing_serial_numbers:
+            raise ValidationError('Такой порядковый номер в данной фотолинии уже существует.')
