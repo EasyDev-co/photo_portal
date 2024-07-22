8858b52da8aee6d055e743aea1a743e35a563372
loach99 <constantinent@mail.ru>
2024-07-22 14:57:33 +0300

    PP-48: убрал функционал подсчета корзины

--- frontend/src/components/Payment/PaymentDiagram/PaymentDiagram.jsx
+++ frontend/src/components/Payment/PaymentDiagram/PaymentDiagram.jsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from 'react';
 import styles from './PaymentDiagram.module.css'
 
 const PaymentDiagram = ({cart}) => {
-    const [count, setCount] = useState(0)
-    useEffect(() => {
-        const sumTotalPrice = cart.reduce((sum, item) => {
-            console.log(parseFloat(item.total_price))
-            return sum + parseFloat(item.total_price);
-        }, 0);
-        setCount(sumTotalPrice);
-    }, [cart]);
+    // const [count, setCount] = useState(0)
+    // useEffect(() => {
+    //     const sumTotalPrice = cart.reduce((sum, item) => {
+    //         console.log(parseFloat(item.total_price))
+    //         return sum + parseFloat(item.total_price);
+    //     }, 0);
+    //     setCount(sumTotalPrice);
+    // }, [cart]);
     return ( 
         <div className={styles.diagramWrap}>
             <span>Итого:</span>
             <div className={styles.diagramCircle}>
-                <div className={styles.count}>{count},00</div>
+                <div className={styles.count}>{0},00</div>
                 <span>рублей</span>
             </div>
         </div>
