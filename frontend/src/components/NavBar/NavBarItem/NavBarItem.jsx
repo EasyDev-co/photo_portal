f1ff29c45a70c3a0ae95d645459179ffd3b7adf2
loach99 <constantinent@mail.ru>
2024-08-07 10:12:39 +0300

    PP-163: добавил в NavBar логику выхода

--- frontend/src/components/NavBar/NavBarItem/NavBarItem.jsx
+++ frontend/src/components/NavBar/NavBarItem/NavBarItem.jsx
@@ -3,11 +3,11 @@ import styles from "./NavBarItem.module.css";
 import { Link } from "react-router-dom";
 import { removeUser } from "../../../store/authSlice";
 
-export const NavBarItem = ({ style, router, icon, alt, text, onClick,isLogOut }) => {
+export const NavBarItem = ({ style, router, icon, alt, text, onClick, isLogOut }) => {
   const dispatch = useDispatch();
-  
+
   return (
-    <li onClick={()=> isLogOut && dispatch(removeUser())} className={`${style ? style : ""}`}>
+    <li onClick={() => isLogOut && dispatch(removeUser())} className={`${style ? style : ""}`}>
       <Link className={styles.link} to={router} onClick={onClick}>
         <img src={icon} alt={alt} />
         <p className={styles.text}>{text}</p>
