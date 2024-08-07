bf9ef9584f24186cc42762915956f7f26da35702
Elena Vagner <develop.vagner@gmail.com>
2024-08-07 22:18:14 +0700

    PP-22: Добавлена проверка активности фототемы при создании/изменении фотолинии

--- backend/apps/photo/models/photo_line.py
+++ backend/apps/photo/models/photo_line.py
@@ -52,6 +52,18 @@ class PhotoLine(UUIDMixin):
 
     def clean(self):
         super().clean()
+
+        # проверяем срок фототемы
         current_time = datetime.now().astimezone(tz=timezone(TIME_ZONE))
         if self.photo_theme.date_end <= current_time:
             raise ValidationError('Срок указанной фототемы вышел.')
+
+        # проверяем соответствует ли переданная фототема активной фототеме д/с
+        if self.photo_theme.is_active:
+            photo_themes = PhotoTheme.objects.filter(
+                is_active=True,
+                photo_lines__kindergarten=self.kindergarten
+            ).distinct()
+
+            if len(photo_themes) == 1 and photo_themes[0] != self.photo_theme or len(photo_themes) > 1:
+                raise ValidationError('Активная тема фотосессии уже есть в этом детском саду.')
