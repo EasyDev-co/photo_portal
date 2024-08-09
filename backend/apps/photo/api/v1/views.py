b47c03e4c158546fc13197a212a0b9285ae20768
Sherif Ragimov <ragimov700@yandex.ru>
2024-08-09 10:24:27 +0400

    feature/PP-152 photo line validation

--- backend/apps/photo/api/v1/views.py
+++ backend/apps/photo/api/v1/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
@@ -49,29 +51,45 @@ class PhotoLineGetByPhotoNumberAPIView(APIView):
     def post(self, request):
         """Получение фотолинии по id"""
 
+        user = request.user
         photo_numbers = request.data['numbers']
         first_photo = get_object_or_404(Photo, number=photo_numbers[0])
         photo_line = first_photo.photo_line
+
+        logging.error(photo_line.kindergarten not in user.kindergarten.all())
+        if photo_line.kindergarten not in user.kindergarten.all():
+            return Response(
+                {'message': 'Фотолиния не относится к вашему детскому саду'},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
         photo_line_photos = photo_line.photos.select_related('photos')
-        numbers_in_photo_line = list(photo_line_photos.values_list('number', flat=True))
+        numbers_in_photo_line = list(
+            photo_line_photos.values_list('number', flat=True)
+        )
+
         if photo_numbers == numbers_in_photo_line:
             serializer = PhotoLineSerializer(photo_line)
 
             user_photo_count, created = UserPhotoCount.objects.get_or_create(
-                user=request.user,
-                photo_theme=photo_line.photo_theme
+                user=user,
+                photo_theme=photo_line.photo_theme,
             )
+
             if user_photo_count.count == 0:
                 return Response(
                     {'message': 'Вы достигли лимита'},
-                    status=status.HTTP_403_FORBIDDEN
+                    status=status.HTTP_403_FORBIDDEN,
                 )
+
             user_photo_count.count -= 1
             user_photo_count.save()
+
             return Response(serializer.data)
+
         return Response(
             {'message': 'Фотолиния с указанными фотографиями не найдена'},
-            status=status.HTTP_404_NOT_FOUND
+            status=status.HTTP_404_NOT_FOUND,
         )
 
 
