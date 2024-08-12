db96fbe96117954dee5015cdbd9bef99c24e3d9d
loach99 <constantinent@mail.ru>
2024-08-12 11:22:13 +0300

    PP-92: внес фиксы

--- frontend/src/components/Orders/PhotoCard/utils/utils.js
+++ frontend/src/components/Orders/PhotoCard/utils/utils.js
@@ -2,32 +2,35 @@ import { useState, useCallback } from "react";
 
 export function transformData(data) {
     const result = [];
+    console.log(data)
+    if (data.length !== 0) {
 
-    const groupedByPhotoLineId = data.reduce((acc, item) => {
-        
-        const { photoLineId, is_photobook, is_digital, ...photo } = item;
-        if (!acc[photoLineId]) {
-            acc[photoLineId] = {
-                id: photoLineId,
-                photos: [],
-                is_photobook: is_photobook,
-                is_digital: is_digital
-            };
-        }
-        
-        if(photo.photo_type != 6){
-            acc[photoLineId].photos.push(photo);
-        } else {
-            acc[photoLineId].photos.push();
-        }  
-        return acc;
-    }, {});
 
-    for (const key in groupedByPhotoLineId) {
-        if (groupedByPhotoLineId.hasOwnProperty(key)) {
-            result.push(groupedByPhotoLineId[key]);
+        const groupedByPhotoLineId = data.reduce((acc, item) => {
+
+            const { photoLineId, is_photobook, is_digital, ...photo } = item;
+            if (!acc[photoLineId]) {
+                acc[photoLineId] = {
+                    id: photoLineId ? photoLineId : photo.id,
+                    photos: [],
+                    is_photobook: is_photobook,
+                    is_digital: is_digital
+                };
+            }
+
+            if (photo.photo_type) {
+                acc[photoLineId].photos.push(photo);
+            }
+            return acc;
+        }, {});
+        console.log(groupedByPhotoLineId)
+        for (const key in groupedByPhotoLineId) {
+            if (groupedByPhotoLineId.hasOwnProperty(key)) {
+                result.push(groupedByPhotoLineId[key]);
+            }
         }
-    }
-    
+
+    } 
+
     return result;
 }
