fa0c0b1bb8c503cb7b9484eaec918886c11ced46
AlexanderPAI <lawlikeart@yandex.ru>
2024-08-20 00:06:16 +0300

    PP-8: Обработка исключений в сервисе ya_disk_api

--- backend/apps/utils/services/ya_disk_api.py
+++ backend/apps/utils/services/ya_disk_api.py
@@ -1,5 +1,6 @@
 import urllib.parse
 import requests
+from http import HTTPStatus
 
 from config.settings import YAD_API_BASE_URL, YAD_OAUTH_TOKEN
 
@@ -15,31 +16,39 @@ class YaDiskApiService:
     @staticmethod
     def convert_path_to_url(path):
         """Преобразовать путь в формат URL."""
-        url = urllib.parse.quote_plus(path)
-        return url
+        return urllib.parse.quote_plus(path)
 
     def get_root_list(self):
         """Получить содержимое корневого каталога приложения."""
         url = self._url + "resources?path=app:/"
         response = requests.get(url=url, headers=self._headers)
-        return response.json()
+        if response.status_code == HTTPStatus.OK:
+            return response.json()
+        return response.json()["description"]
 
     def get_upload_url(self, upload_path):
         """Создать ссылку для загрузки файла."""
         url = self._url + "resources/upload?path=" + upload_path
         response = requests.get(url=url, headers=self._headers)
-        return response.json()["href"]
+        if response.status_code == HTTPStatus.OK:
+            return response.json()["href"]
+        return {"message": f"не удалось получить ссылку для загрузки: {url}"}
 
     def upload_file(self, upload_url, file):
         """Загрузка одного файла на Яндекс диск."""
-        requests.put(url=upload_url, headers=self._headers, data=file)
+        response = requests.put(url=upload_url, headers=self._headers, data=file)
+        if response.status_code == HTTPStatus.OK:
+            return response.json()
+        return {"message": f"не удалось загрузить файл {upload_url}"}
 
     def get_folder_meta(self, path):
         """Получить мета-данные папки (для проверки ее наличия)."""
         path = self.convert_path_to_url(path)
         url = self._url + "resources?path=" + path
         response = requests.get(url=url, headers=self._headers)
-        return response.json()
+        if response.status_code == HTTPStatus.OK:
+            return response.json()
+        return {"message": f"не удалось получить мета-данные каталога {path}"}
 
     def get_or_create_folder(self, path):
         """Получить URL или создать папку (при ее отсутствии)."""
