916bb93a88dd8e2733376101616cdf05ba48317d
Sherif Ragimov <ragimov700@yandex.ru>
2024-08-22 17:13:03 +0400

    feature/PP-178 custom password validator

--- backend/apps/user/validators.py
+++ backend/apps/user/validators.py
@@ -1,6 +1,28 @@
+import re
+
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
 validate_cyrillic = RegexValidator(
     regex=r'^[А-Яа-яЁё]+$',
     message='Можно вводить только кириллические символы.'
 )
+
+
+class CustomPasswordValidator:
+    """
+    Кастомный валидатор пароля.
+    """
+
+    def validate(self, password, user=None):
+        if not re.match(r'^[a-zA-Z0-9~!@#$%^&*()_\-+=]+$', password):
+            raise ValidationError(
+                "Пароль может содержать только латинские буквы, цифры "
+                "и спецсимволы ~!@#$%^&*()_-+=."
+            )
+
+    def get_help_text(self):
+        return (
+            "Пароль должен содержать только латинские буквы, цифры и "
+            "спецсимволы ~!@#$%^&*()_-+=."
+        )
