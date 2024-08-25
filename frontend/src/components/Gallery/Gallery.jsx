fa6996c14bdeb937ef2c07604ab39a272e29feb1
loach99 <constantinent@mail.ru>
2024-08-25 13:36:50 +0300

    feature/PP-171: добавил в галерею оплаченый заказ

--- frontend/src/components/Gallery/Gallery.jsx
+++ frontend/src/components/Gallery/Gallery.jsx
@@ -1,32 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Gallery.module.css";
 import GalleryItem from "./GalleryItem";
 import { useSelector } from "react-redux";
 import { fetchStatePaymentTokenInterceptor, statePayment } from "../../http/statePayment";
+import { fetchGetPaidOrderTokenInterceptor } from "../../http/getPaidOrders";
 export const Gallery = () => {
   const accessStor = localStorage.getItem('access');
   const order = useSelector(state => state.user.order);
-
+  const [paidOrders, setPaidOrders] = useState([])
   useEffect(() => {
     try {
       fetchStatePaymentTokenInterceptor(accessStor)
         .then(res => {
-          if(res.ok){
+          if (res.ok) {
             res.json()
-              .then(res=>{
+              .then(res => {
                 console.log(res)
-              }) 
+              })
           }
         })
     } catch (error) {
       console.log(error)
     }
-    
+  }, [])
+  useEffect(() => {
 
+    fetchGetPaidOrderTokenInterceptor(accessStor)
+      .then(res=>{
+        if(res.ok){
+          res.json()
+            .then(res=>{
+              setPaidOrders(res)
+            })
+        }
+      })
   }, [])
   return <>
     <div className={styles.ordersWrap}>
-      <GalleryItem />
+      <GalleryItem
+        orders={paidOrders} 
+      />
     </div>
   </>;
 };
