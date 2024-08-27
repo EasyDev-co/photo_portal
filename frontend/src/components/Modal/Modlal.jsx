ee39ab76d5a960ee7a426d5c8dcac892add00849
loach99 <constantinent@mail.ru>
2024-08-27 18:00:58 +0300

    PP-43: изменил стилизацию модального окна

--- frontend/src/components/Modal/Modlal.jsx
+++ frontend/src/components/Modal/Modlal.jsx
@@ -1,10 +1,18 @@
 import style from './Modal.module.css'
-
+import danger from '../../assets/images/Auth/DangerCircle.svg'
 const Modal = ({ active, setActive }) => {
     return (
         <div className={active ? style.modal__active : style.modal} onClick={() => setActive(false)}>
             <div className={style.modal__content}>
-                Вы достигли лимита, если у вас четверо детей , обратитесь к администратору
+                <div className={style.modal__text}>
+                    <img src={danger} alt="" />
+                    <p>
+                        Вы превысили лимит добавления детей. Если у вас четверо детей, то напишите нам на
+                        <span> fotodetstvo1@yandex.ru </span>
+                        и мы проверим информацию
+                    </p>
+                </div>
+
             </div>
         </div>
     );
