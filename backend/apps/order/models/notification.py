b6258d11b2d0acc5534bb3446b688db7f786147f
Elena Vagner <develop.vagner@gmail.com>
2024-09-11 20:38:56 +0700

    PP-207: Добавлены модели нотификации и чека.

---
+++ backend/apps/order/models/notification.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from apps.utils.models_mixins.models_mixins import UUIDMixin, TimeStampedMixin
+
+
+class NotificationFiscalization(UUIDMixin, TimeStampedMixin):
+    notification = models.JSONField(
+        default=dict
+    )
+    was_processed = models.BooleanField(
+        default=False
+    )
+
+    class Meta:
+        verbose_name = 'Нотификация о фискализации'
+        verbose_name_plural = 'Нотификации о фискализации'
