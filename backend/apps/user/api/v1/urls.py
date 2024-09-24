701870aae7e2f3396d043e6e6779dd969e1d49ef
Elena Vagner <develop.vagner@gmail.com>
2024-09-24 21:59:35 +0700

    PP-219: Добавлен апи для получения бонуса заведующей

--- backend/apps/user/api/v1/urls.py
+++ backend/apps/user/api/v1/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 
-from apps.user.api.v1.views import UserAPIView
+from apps.user.api.v1.views import UserAPIView, ManagerBonusAPIView
 
 urlpatterns = [
     path('parent/', include('apps.user.api.v1.parent.urls')),
-
     path('user/', UserAPIView.as_view(), name='user'),
+    path('manager_bonus/', ManagerBonusAPIView.as_view(), name='manager_bonus'),
 ]
