b652d302167f4544e7a2168baa1db0ecf026bc62
Vlad <dr.melix@gmail.com>
2024-10-11 00:59:06 +0300

    PP-oauth: маршрутизация

---
+++ backend/apps/oauth/api/v1/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .providers import google, yandex, mailru
+from . import views
+
+urlpatterns = [
+    # Google URLs
+    path('login/google/', google.google_login, name='oauth_google_login'),
+    path('callback/google/', google.google_callback, name='oauth_google_callback'),
+
+    # Yandex URLs
+    path('login/yandex/', yandex.yandex_login, name='oauth_yandex_login'),
+    path('callback/yandex/', yandex.yandex_callback, name='oauth_yandex_callback'),
+
+    # Mail.ru URLs
+    path('login/mailru/', mailru.mailru_login, name='oauth_mailru_login'),
+    path('callback/mailru/', mailru.mailru_callback, name='oauth_mailru_callback'),
+
+    # Logout
+    path('logout/', views.logout_view, name='oauth_logout'),
+]
