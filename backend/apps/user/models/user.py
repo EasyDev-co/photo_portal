81a878b7ce5dbb1d24f0264f544d9002c45eed1d
alexalkhov <alex_0725@mail.ru>
2024-10-10 14:29:55 +0300

    fix code

--- backend/apps/user/models/user.py
+++ backend/apps/user/models/user.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
 from django.db import models
@@ -71,7 +71,6 @@ class User(UUIDMixin, AbstractUser):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     class Meta:
