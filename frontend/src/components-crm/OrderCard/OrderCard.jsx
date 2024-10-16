cc32788ebf04824cf6e786d4a40ad43fd8015d2a
isaSyr <lisabenamisa@yahoo.com>
2024-10-16 11:27:09 +0300

    feature/ui-fixes-1: client card ui fixes

--- frontend/src/components-crm/OrderCard/OrderCard.jsx
+++ frontend/src/components-crm/OrderCard/OrderCard.jsx
@@ -8,6 +8,17 @@ import LocationMap from "../YMap/LocationMap";
 
 const OrderCard = ({region, city}) => {
     const [isActive, setIsActive] = useState(false);
+    const [showInfo, setShowInfo] = useState(false)
+    const [showMap, setShowMap] = useState(false)
+
+    const handleInfoShow =()=>{
+        setShowInfo(prev => !prev)
+    }
+
+    const handleMapShow =()=>{
+        setShowMap(prev=> !prev)
+    }
+
     return (
 
         <Card className="border-0 d-flex flex-column"
@@ -20,11 +31,14 @@ const OrderCard = ({region, city}) => {
                 gap: '24px'
             }}>
                 <Card.Header style={{
-                    fontSize: '20px'
+                    fontSize: '17px'
                 }} className="border-0 fw-600 p-0">
                     Местонахождение
+                    <div className="cursor-pointer">
+                        <button  onClick={handleMapShow}> <img src={addButton} alt=""  /></button>
+                    </div>
                 </Card.Header>
-                <Card.Body className="p-0 d-flex gap-3 flex-column">
+                {showMap&&<Card.Body className="p-0 d-flex gap-3 flex-column">
                     <div className="map-wrap border " style={{
                         borderRadius: '8px'
                     }}>
@@ -34,39 +48,39 @@ const OrderCard = ({region, city}) => {
                     </div>
                     <div className="d-flex gap-3">
                         <Form.Group className="flex-grow-1">
-                            <Form.Label className="text-secondary">Регион</Form.Label>
+                            <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Регион</Form.Label>
                             <Form.Control className='shadow-none ps-3' placeholder={`${region}`} />
                         </Form.Group>
                         <Form.Group className="flex-grow-1">
-                            <Form.Label className="text-secondary">Город</Form.Label>
+                            <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Город</Form.Label>
                             <Form.Control className='shadow-none ps-3' placeholder={`${city}`} />
                         </Form.Group>
                         <Form.Group className="flex-grow-1">
-                            <Form.Label className="text-secondary">Адрес</Form.Label>
+                            <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Адрес</Form.Label>
                             <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
                         </Form.Group>
                     </div>
-                </Card.Body>
+                </Card.Body>}
             </Card>
             <Card className="border-0 p-0" style={{
                 gap: '24px'
             }}>
                 <Card.Header style={{
-                    fontSize: '20px',
+                    fontSize: '17px',
                 }} className="border-0 fw-600 p-0">
                     Контактная информация
                     <div className="cursor-pointer">
-                        <img src={addButton} alt="" />
+                        <button  onClick={handleInfoShow}> <img src={addButton} alt=""  /></button>
                     </div>
                 </Card.Header>
-                <Card.Body className="p-3 border" style={{
+                {showInfo&&<Card.Body className="p-3 border" style={{
                     borderRadius: '8px'
                 }}>
                     <div className="d-flex flex-column gap-3">
                         <div className="d-flex gap-3">
                             <div className="flex-grow-1 d-flex flex-column gap-3 justify-content-between">
                                 <Card className="border-0 p-2">
-                                    <Card.Header className="border-0 fw-600 p-0" style={{ fontSize: '20px' }}>
+                                    <Card.Header className="border-0 fw-600 p-0" style={{ fontSize: '17px' }}>
                                         Марк Ифанасьев
                                     </Card.Header>
                                     <Card.Title className="fs-6 text-secondary">
@@ -75,7 +89,7 @@ const OrderCard = ({region, city}) => {
                                 </Card>
                                 <Card className="border-0">
                                     <Form.Group className="">
-                                        <Form.Label className="text-secondary">Почта</Form.Label>
+                                        <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Почта</Form.Label>
                                         <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
                                     </Form.Group>
                                 </Card>
@@ -83,12 +97,12 @@ const OrderCard = ({region, city}) => {
                             <div className="flex-grow-1 d-flex flex-column gap-3">
                                 <Card className="border-0">
                                     <Form.Group className="">
-                                        <Form.Label className="text-secondary">Номер</Form.Label>
+                                        <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Номер</Form.Label>
                                         <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
                                     </Form.Group>
                                 </Card>
                                 <Card className="border-0">
-                                    <Form.Group className="">
+                                    <Form.Group className="" style={{ fontSize: '15px' }}>
                                         <DatePicker
                                             label={'День рождения заведующего'}
                                             placeholder={'Не указано'}
@@ -106,32 +120,32 @@ const OrderCard = ({region, city}) => {
                         <div>
                             <Card className="border-0 gap-3">
                                 <div className="fw-500 text-center" style={{
-                                    fontSize: '18px'
+                                    fontSize: '15px'
                                 }}>Логин заведующего</div>
                                 <div className="d-flex gap-3">
                                     <Form.Group className="flex-grow-1">
-                                        <Form.Label className="text-secondary">Логин</Form.Label>
+                                        <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Логин</Form.Label>
                                         <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
                                     </Form.Group>
                                     <Form.Group className="flex-grow-1">
-                                        <Form.Label className="text-secondary">Пароль</Form.Label>
+                                        <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Пароль</Form.Label>
                                         <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
                                     </Form.Group>
                                 </div>
                                 <Form.Group className="flex-grow-1">
-                                    <Form.Label className="text-secondary">Промокод на скидку</Form.Label>
+                                    <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Промокод на скидку</Form.Label>
                                     <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
                                 </Form.Group>
                             </Card>
                         </div>
                     </div>
-                </Card.Body>
+                </Card.Body>}
             </Card>
             <Card className="border-0 p-0" style={{
                 gap: '24px'
             }}>
                 <Card.Header style={{
-                    fontSize: '20px',
+                    fontSize: '17px',
                 }} className="border-0 fw-600 p-0">
                     Сбор заказа
                 </Card.Header>
@@ -150,7 +164,7 @@ const OrderCard = ({region, city}) => {
                     </Form.Group>
                     <div className="d-flex align-items-end gap-3 flex-grow-1">
                         <Form.Group className="flex-grow-1">
-                            <Form.Label className="text-secondary">Пароль</Form.Label>
+                            <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Пароль</Form.Label>
                             <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
                         </Form.Group>
                         <Form.Check
@@ -161,41 +175,17 @@ const OrderCard = ({region, city}) => {
                     </div>
                 </Card.Body>
             </Card>
+
             <Card className="border-0 p-0" style={{
                 gap: '24px'
             }}>
                 <Card.Header style={{
-                    fontSize: '20px',
-                }} className="border-0 fw-600 p-0">
-                    Другое
-                </Card.Header>
-                <Card.Body className="p-0 d-flex flex-column gap-3">
-                    <div className="d-flex gap-3">
-                        <Form.Group className="flex-grow-1">
-                            <Form.Label className="text-secondary">Количество детей</Form.Label>
-                            <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
-                        </Form.Group>
-                        <Form.Group className="flex-grow-1">
-                            <Form.Label className="text-secondary">Количество детей на фотосессию</Form.Label>
-                            <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
-                        </Form.Group>
-                    </div>
-                    <Form.Group className="flex-grow-1">
-                        <Form.Label className="text-secondary">Реквизиты</Form.Label>
-                        <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
-                    </Form.Group>
-                </Card.Body>
-            </Card>
-            <Card className="border-0 p-0" style={{
-                gap: '24px'
-            }}>
-                <Card.Header style={{
-                    fontSize: '20px',
+                    fontSize: '17px',
                 }} className="border-0 fw-600 p-0">
                     Фотосессии
                 </Card.Header>
                 <div className="d-flex justify-content-between px-4">
-                    <div className="text-secondary fw-600">
+                    <div className="text-secondary fw-600" style={{ fontSize: '15px' }}>
                         Дата
                     </div>
 
@@ -203,16 +193,16 @@ const OrderCard = ({region, city}) => {
                 <Card.Body className="p-0 gap-2 d-flex flex-column scroll-body" style={{
                     height: '230px'
                 }}>
-                    <div className="call-item pe-5">
+                    <div className="call-item pe-5" style={{ fontSize: '15px' }}>
                         26.07.2024
                     </div>
-                    <div className="call-item pe-5">
+                    <div className="call-item pe-5" style={{ fontSize: '15px' }}>
                         26.07.2024
                     </div>
-                    <div className="call-item pe-5">
+                    <div className="call-item pe-5" style={{ fontSize: '15px' }}>
                         26.07.2024
                     </div>
-                    <div className="call-item pe-5">
+                    <div className="call-item pe-5" style={{ fontSize: '15px' }}>
                         26.07.2024
                     </div>
                 </Card.Body>
@@ -221,45 +211,71 @@ const OrderCard = ({region, city}) => {
                 gap: '24px'
             }}>
                 <Card.Header style={{
-                    fontSize: '20px',
+                    fontSize: '17px',
                 }} className="border-0 fw-600 p-0">
                     Звонки
                 </Card.Header>
                 <div className="d-flex justify-content-between px-4">
-                    <div className="text-secondary fw-600">
+                    <div className="text-secondary fw-600" style={{ fontSize: '15px' }}>
                         Дата
                     </div>
-                    <div className="text-secondary fw-600 pe-5">
+                    <div className="text-secondary fw-600 pe-5" style={{ fontSize: '15px' }}>
                         Результат
                     </div>
                 </div>
                 <Card.Body className="p-0 gap-2 d-flex flex-column scroll-body" style={{
                     height: '230px'
                 }}>
-                    <div className="call-item pe-5">
+                    <div className="call-item pe-5" style={{ fontSize: '15px' }}>
                         26.07.2024
                         <div>
                             <Badge bg="success text-black" >Записан на фотосессию</Badge>
                         </div>
                     </div>
-                    <div className="call-item pe-5">
+                    <div className="call-item pe-5" style={{ fontSize: '15px' }}>
                         26.07.2024
                         <div>
                             <Badge bg="success" className="text-black" >Записан на фотосессию</Badge>
                         </div>
                     </div>
-                    <div className="call-item pe-5">
+                    <div className="call-item pe-5" style={{ fontSize: '15px' }}>
                         26.07.2024
                         <div>
                             <Badge bg="warning text-black" >Отправлено коммерческое предложение</Badge>
                         </div>
                     </div>
-                    <div className="call-item pe-5">
+                    <div className="call-item pe-5" style={{ fontSize: '15px' }}>
                         26.07.2024
                         <div>
                             <Badge bg="danger text-black" >Отказ</Badge>
                         </div>
                     </div>
+                </Card.Body>
+            </Card>
+
+            <Card className="border-0 p-0" style={{
+                gap: '24px'
+            }}>
+                <Card.Header style={{
+                    fontSize: '17px',
+                }} className="border-0 fw-600 p-0">
+                    Другое
+                </Card.Header>
+                <Card.Body className="p-0 d-flex flex-column gap-3">
+                    <div className="d-flex gap-3">
+                        <Form.Group className="flex-grow-1">
+                            <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Количество детей</Form.Label>
+                            <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
+                        </Form.Group>
+                        <Form.Group className="flex-grow-1">
+                            <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Количество детей на фотосессию</Form.Label>
+                            <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
+                        </Form.Group>
+                    </div>
+                    <Form.Group className="flex-grow-1">
+                        <Form.Label className="text-secondary" style={{ fontSize: '15px' }}>Реквизиты</Form.Label>
+                        <Form.Control className='shadow-none ps-3' placeholder='Не указано' />
+                    </Form.Group>
                 </Card.Body>
             </Card>
         </Card>
