78d485f14ad43cc4c3005666d175ad3712c0dc8f
Vlad <dr.melix@gmail.com>
2024-10-25 15:24:01 +0300

    Revert "Update OAuth.js"
    This reverts commit 888561328b48a71dcab2c7bdb3c6bd89a6237712.

--- frontend/src/http/OAuth/OAuth.js
+++ frontend/src/http/OAuth/OAuth.js
@@ -1,37 +1,30 @@
 import { localUrl } from "../../constants/constants";
 
 export const OAuth = async (provider) => {
+
     const url = `${localUrl}/api/oauth/v1/login/${provider}`;
     const response = await fetch(url, {
         method: "GET",
+        mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
         }
     });
-
-    // Проверка ответа
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-
-    return response.json(); // или просто return response, если вам нужно полное тело ответа
+    return response;
 }
 
 export const OAuthGetToken = async (data) => {
+
     const url = `${localUrl}/api/oauth/v1/login/oauth_token/`;
     const response = await fetch(url, {
         method: "POST",
+        // mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
-            // 'Authorization': `Bearer ${access}` // если нужно
+            // 'Authorization': `Bearer ${access}`
         },
-        body: JSON.stringify(data)
+        body:JSON.stringify(data)
     });
-
-    // Проверка ответа
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-
-    return response.json(); // или просто return response, если вам нужно полное тело ответа
+    return response;
 }
+
