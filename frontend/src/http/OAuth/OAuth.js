888561328b48a71dcab2c7bdb3c6bd89a6237712
Vlad Melix <dr.melix@gmail.com>
2024-10-25 14:56:36 +0300

    Update OAuth.js

--- frontend/src/http/OAuth/OAuth.js
+++ frontend/src/http/OAuth/OAuth.js
@@ -1,30 +1,37 @@
 import { localUrl } from "../../constants/constants";
 
 export const OAuth = async (provider) => {
-
     const url = `${localUrl}/api/oauth/v1/login/${provider}`;
     const response = await fetch(url, {
         method: "GET",
-        mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
         }
     });
-    return response;
+
+    // Проверка ответа
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    return response.json(); // или просто return response, если вам нужно полное тело ответа
 }
 
 export const OAuthGetToken = async (data) => {
-
     const url = `${localUrl}/api/oauth/v1/login/oauth_token/`;
     const response = await fetch(url, {
         method: "POST",
-        // mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
-            // 'Authorization': `Bearer ${access}`
+            // 'Authorization': `Bearer ${access}` // если нужно
         },
-        body:JSON.stringify(data)
+        body: JSON.stringify(data)
     });
-    return response;
-}
 
+    // Проверка ответа
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    return response.json(); // или просто return response, если вам нужно полное тело ответа
+}
