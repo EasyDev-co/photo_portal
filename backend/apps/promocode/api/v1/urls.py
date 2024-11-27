ee3798d3da12ab3940777e4a5369ff305d476bcd
Elena Vagner <develop.vagner@gmail.com>
2024-11-27 19:51:56 +0700

    Получение промокода по API на странице заведующей

--- backend/apps/promocode/api/v1/urls.py
+++ backend/apps/promocode/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from apps.promocode.api.v1.views import PromocodeRetrieveAPIView
-
+from apps.promocode.api.v1.views import PromocodeRetrieveAPIView, PromocodeAPIView
 
 urlpatterns = [
-    path('get/', PromocodeRetrieveAPIView.as_view(), name="promocode")
+    path('get/', PromocodeRetrieveAPIView.as_view(), name="promocode"),
+    path('get_manager_promocode/', PromocodeAPIView.as_view(), name="promocode")
 ]
