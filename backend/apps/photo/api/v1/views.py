c23c6893105db7c0ea92aba770da6c8607a64b49
tkazakov <tkazakov@zigmund.online>
2025-04-10 18:57:15 +0300

    Исправление сортировки

--- backend/apps/photo/api/v1/views.py
+++ backend/apps/photo/api/v1/views.py
@@ -115,7 +115,7 @@ class PhotoLinesGetByParent(ListAPIView):
         return PhotoLine.objects.filter(
             parent=parent,
             photo_theme__kindergartenphototheme__is_active=True
-        ).order_by('child_number')
+        ).order_by('parent_photo_lines__child_number')
 
 
 class CurrentPhotoThemeRetrieveAPIView(RetrieveAPIView):
